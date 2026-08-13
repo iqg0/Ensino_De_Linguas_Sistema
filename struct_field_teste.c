12e25a93c8deff69c2619d9b1b6a3629af64c5ab
Iago <iagocamargo95@gmail.com>
2026-08-13 14:22:35 -0300

    Relembrando como usar struct fields para o projeto

---
+++ struct_field_teste.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+    typedef struct {
+        char Aluno[50];
+        int IdAluno;
+    }Escola;
+
+    
+    typedef struct {
+        char Materia[50];
+        Escola NomeAlunos[5];
+        int NumeroSala;
+    }Curso;
+
+int main(){
+
+    Curso MeuCurso;
+    strcpy(MeuCurso.NomeAlunos[0].Aluno, "Gabriel");
+
+
+    return 0;
+}
