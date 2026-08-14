33b828db3df84ef3659b6130038b8f5b82984d45
Iago <iagocamargo95@gmail.com>
2026-08-14 12:51:53 -0300

    teste

--- struct_field_teste.c
+++ struct_field_teste.c
@@ -11,16 +11,19 @@
         char Materia[50];
         Escola NomeAlunos[5];
         int NumeroSala;
+        int totalAlunos;
     }Curso;
 
 int main(){
 
     Curso MeuCurso;
     strcpy(MeuCurso.NomeAlunos[0].Aluno, "Gabriel");
+    MeuCurso.totalAlunos = 1;
     MeuCurso.NomeAlunos[0].IdAluno = 19;
 
-    for(int i = 0; i == 0; i++){
-        printf("Nome: %s, Id: %d",
+
+    for(int i = 0; i < MeuCurso.totalAlunos; i++){
+        printf("Nome: %s, Id: %d\n",
                 MeuCurso.NomeAlunos[i].Aluno,
                 MeuCurso.NomeAlunos[i].IdAluno);
     }
