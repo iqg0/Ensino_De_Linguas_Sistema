ba1dc59132a68555c90c09c39a285377bdb3fcef
Iago <iagocamargo95@gmail.com>
2026-08-16 22:12:38 -0300

    Teste

--- project_teste.c
+++ project_teste.c
@@ -4,6 +4,7 @@
     typedef struct{
         char nome[50];
         int id;
+        int nota;
     }Aluno;
 
     typedef struct {
@@ -11,20 +12,22 @@
         char serie[50];
         Aluno aluno[30];
         int totalAluno;
-        int nota;
     }Turma;
 
 int main() {
 
     Turma Curso;
+
+    int testeC2 = 0;
+    
     printf("Informe o nome do aluno: ");
     scanf("%49s", Curso.aluno[0].nome);
-    
 
-
-
+    printf("Em qual turma(idioma) esse aluno se encontra matriculado: ");
+    scanf("%49s", Curso.idioma);
 
     printf("Nome: %s\n", Curso.aluno[0].nome);
+    printf("Curso: %s", Curso.idioma[0]);
 
     return 0;
 }
